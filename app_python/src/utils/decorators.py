a89aae5e921c1f1690b968be9ed101d678611cfb
joao.axer <joao.axer@vallourec.com>
2026-01-25 17:34:27 -0300

    feat: Criar decorator para capturar tempo de execução dos handlers

---
+++ app_python/src/utils/decorators.py
@@ -0,0 +1,16 @@
+import logging
+import time
+from functools import wraps
+
+logger = logging.getLogger(__name__)
+
+def tempo_execucao(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        inicio = time.time()
+        resultado = func(*args, **kwargs)
+        fim = time.time()
+        tempo_execucao = (fim - inicio) * 1000
+        logger.info(f"{func.__name__} executou em {tempo_execucao:.5f} ms")
+        return resultado
+    return wrapper
